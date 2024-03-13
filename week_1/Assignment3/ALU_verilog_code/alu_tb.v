67efaf8596ea8e239c876d333470b8c7163dfb6f
jaya117 <139655462+jaya117@users.noreply.github.com>
2024-03-13 00:09:44 -0500

    Updated alu_tb.v

--- week_1/Assignment3/ALU_verilog_code/alu_tb.v
+++ week_1/Assignment3/ALU_verilog_code/alu_tb.v
@@ -1,10 +1,12 @@
+
 module alu_tb;
 
    reg  [2:0]inst;
    reg  [3:0]A,B;
    wire [3:0]res;
+   wire ovflw;
    
-   alu alu_inst(.instruction(inst),.operand_a(A),.operand_b(B),.result(res));
+  alu alu_inst(.opcode(inst),.operand_a(A),.operand_b(B),.result(res),.overflow(ovflw));
    
    integer i;
    
@@ -12,8 +14,8 @@ module alu_tb;
      $dumpfile("waveforms.vcd");
      $dumpvars(0,alu_tb);
    	   
-     $monitor("monitor A:%0b B:%0b inst:%0b res:%0b @ %0t", A, B, inst, res, $time);
-     A = 7; B = 8; inst = 0;
+     $monitor("monitor A:%0b B:%0b inst:%0b overflow:%0b result:%0b @ %0t", A, B, inst, ovflw, res, $time);
+     A = 8; B = 7; inst = 0;
    
      for(i = 1; i < 8; i = i+ 1)begin
        #10
