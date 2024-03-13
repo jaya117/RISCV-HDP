72523b934fbdd1eb20d4abefa396d3c574460403
jaya117 <139655462+jaya117@users.noreply.github.com>
2024-03-13 00:11:53 -0500

    Updated alu.v
    added overflow bit

--- week_1/Assignment3/ALU_verilog_code/alu.v
+++ week_1/Assignment3/ALU_verilog_code/alu.v
@@ -1,29 +1,31 @@
 module alu(
 input [2:0]opcode,
 input [3:0]operand_a,operand_b,
-output reg [3:0]result
+  output reg [3:0]result,
+  output reg overflow
     );
     
    always @(*)begin
-      case(instruction)
+     case(opcode)
         3'b000:
-            result = operand_a + operand_b;
+        {overflow, result} = operand_a + operand_b;
         3'b001:
-            result = operand_a - operand_b;
+        {overflow,result} = operand_a - operand_b;
         3'b010:
-            result = operand_a / operand_b;
+        {overflow,result} = operand_a / operand_b;
         3'b011:
-            result = operand_a * operand_b;  
+        {overflow,result} = operand_a * operand_b;  
         3'b100:
-            result = operand_a & operand_b; 
+        {overflow,result} = operand_a & operand_b; 
         3'b101:
-            result = operand_a | operand_b;
+        {overflow,result} = operand_a | operand_b;
         3'b110:
-            result = ~operand_a;
+        {overflow,result} = ~operand_a;
         3'b111:
-        result = operand_a ^ operand_b;
+        {overflow,result} = operand_a ^ operand_b;
         default:
-       result = 0;
+        {overflow,result}='h0;
     endcase
 end
 
+endmodule
